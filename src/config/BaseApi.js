addef5f3546bd101188746c53d7605045b222115
mahathir mohamed <mahathirmohamed791@gmail.com>
2023-04-19 14:43:15 +0530

    baseurl changed

--- src/config/BaseApi.js
+++ src/config/BaseApi.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://bloggers-world.onrender.com";
-export const baseUrl = "http://192.168.1.12:8000";
+export const baseUrl = "https://bloggers-world.onrender.com";
+// export const baseUrl = "http://192.168.1.12:8000";
