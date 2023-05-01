c009c2d71c70506de9f27f5dbf098da514a6836e
mahathir mohamed <mahathirmohamed791@gmail.com>
2023-05-01 18:32:57 +0530

    pagination added

--- src/Components/BlogComponents/AllPost.js
+++ src/Components/BlogComponents/AllPost.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect,useRef} from 'react';
 import PostCard from '../../Helpers/PostCard';
 import Container from 'react-bootstrap/Container'
 import axios from 'axios';
@@ -7,6 +7,7 @@ import AOS from 'aos';
 import Cookies from 'js-cookie';
 import {baseUrl} from "../../config/BaseApi";
 import {useSelector} from 'react-redux';
+import {toast,ToastContainer} from 'react-toastify';
 
 export default function AllPost() {
   const override: CSSProperties = {
@@ -20,32 +21,40 @@ export default function AllPost() {
    const [Blog,setBlog]=useState();
    const [loading,setLoading]=useState(true);
    const [likes,setlikes]=useState();
+   const [Page,setPage] = useState(1);
+   const [Limit,setLimit] = useState(3);
    const userData = useSelector((state)=>state.user.userDetail);
-
+   const PrevIndex = useRef(1);
+   const [maxPage,setmaxPage]=useState();
+   const[length,setLength]=useState()
    const user = Cookies.get("user_id").replace(/"|'/g, '');
     useEffect(()=>{
-        axios.get(`${baseUrl}/all-post`).then((response)=>{
-          setBlog(response.data);
-          if(Blog){
-            setLoading(false);
-          }
-        }).catch((error)=>{console.log(error)})
+        fetchPosts();
         AOS.init();
-    },[]);
+        // console.log(PrevIndex.current);
+    },[Page]);
+    useEffect(()=>{
+      const MaxPage = Math.floor(length/Limit)+1;
+      setmaxPage(MaxPage);
+      console.log(MaxPage);
+    },[length,Limit])
     useEffect(()=>{
       setlikes(userData.likes)
     },[userData]);
+    useEffect(() => {
+    PrevIndex.current = Page;
+  },[length]);
 
-      // useEffect(()=>{
-      //   setlikes(likes);
-      //   console.log(likes)
-      // },[likes])
-    // async function FetchUser(){
-    //     await axios.get(`${baseUrl}/auth/FindUser/${user}`).then((res)=>{
-    //       console.log(res.data)
-    //       setlikes(res.data.result.likes)
-    //     }).catch((err)=>{console.log(err)});
-    // }
+  function fetchPosts(){
+    axios.get(`${baseUrl}/all-post?page=${Page}&limit=${Limit}`).then((response)=>{
+      setBlog(response.data.result);
+      setLength(response.data.TotalPost)
+      if(Blog){
+            setLoading(false);
+          }
+      }).catch((error)=>{console.log(error)})
+  }
+
   return (
     <Container className="test">
         <div className="d-flex justify-content-center" style={{width:"100%",padding:20}}>
@@ -66,10 +75,29 @@ export default function AllPost() {
         data-testid="loader"
       />}
 
-      {/* <div style={{width:"100%",display:"flex",justifyContent:"center",alignItems:"center"}}><lottie-player src="https://assets10.lottiefiles.com/private_files/lf30_e3pteeho.json"  background="transparent"  speed="1" style={{width:300,height:300}}  loop  autoplay></lottie-player></div> */}
-         
-            
+      {/* <div style={{width:"100%",display:"flex",justifyContent:"center",alignItems:"center"}}><lottie-player src="https://assets10.lottiefiles.com/private_files/lf30_e3pteeho.json"  background="transparent"  speed="1" style={{width:300,height:300}}  loop  autoplay></lottie-player></div> */}   
         </div>
+        <div className="d-flex justify-content-around w-100">
+           <input type="button"onClick={()=>{
+            if(Page>1){
+               setPage(Page-1);
+               fetchPosts();
+            }else{
+              toast.info("This is end of the page",{position:toast.POSITION.BOTTOM_CENTER})
+            }
+            
+            PrevIndex.current = Page;
+            }}   className="btn btn-primary" value="Prev"/>
+           <input type="button" onClick={()=>{
+            if(Page<maxPage){
+              setPage(Page+1);
+              fetchPosts();
+            }else{
+              toast.info("No more pages left",{position:toast.POSITION.BOTTOM_CENTER})
+            }
+            }}  className="btn btn-primary" value="Next"/>
+        </div>
+         <ToastContainer/>
     </Container>
   )
 }
